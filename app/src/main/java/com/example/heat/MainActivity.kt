093f0f10809ce110d103a0a4ec8e89a4268c7902
Aref Yazdkhasti <aref.yazdkhasti@gmail.com>
2022-08-10 13:11:08 +0430

    run project properly and add needed strings

--- app/src/main/java/com/example/heat/MainActivity.kt
+++ app/src/main/java/com/example/heat/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.heat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.heat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //first method for finding navController throw error in navigation-fragment-ktx:2.5.1 or higher
+        //navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        findNavController(R.id.nav_host_fragment).navigateUp()
+
 }
