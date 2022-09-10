1ee822fe660f4f9fce4c2bcadff0cd02e1c01c06
Aref Yazdkhasti <aref.yazdkhasti@gmail.com>
2022-09-10 22:03:08 +0430

    add recipe cal amount to recipeItemRecyclerView

--- app/src/main/java/com/example/heat/ui/itemRecyclerView/RecipeItemRecyclerView.kt
+++ app/src/main/java/com/example/heat/ui/itemRecyclerView/RecipeItemRecyclerView.kt
@@ -1,5 +1,6 @@
 package com.example.heat.ui.itemRecyclerView
 
+import android.annotation.SuppressLint
 import android.view.animation.AnimationUtils
 import com.example.heat.R
 import com.example.heat.data.datamodel.food.foodSummery.FoodSummery
@@ -17,6 +18,7 @@ class RecipeItemRecyclerView(val foodSummery: FoodSummery, val type: RecipeViewT
     override fun getLayout(): Int =
         if (type == RecipeViewType.LARGE) R.layout.recipe_item_horizontal else R.layout.recipe_item
 
+    @SuppressLint("SetTextI18n")
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
 
         val binding = RecipeItemHorizontalBinding.bind(viewHolder.itemView)
@@ -25,7 +27,7 @@ class RecipeItemRecyclerView(val foodSummery: FoodSummery, val type: RecipeViewT
         binding.apply {
             recipeImage.clipToOutline = true
             recipeName.text = foodSummery.title
-
+            recipeCal.text = "${foodSummery.calorie.amount} ${foodSummery.calorie.unit}"
             recipeRoot.animation =
                 AnimationUtils.loadAnimation(viewHolder.itemView.context, R.anim.recycler_item_anim)
         }
